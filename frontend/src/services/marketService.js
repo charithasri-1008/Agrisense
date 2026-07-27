d3d6b07b75057aa7cd296a974ff50a338d5466f6
Charitha Sri <charithadornala@gmail.com>
2026-07-27 12:27:27 +0530

    Fix market service production API URL

--- frontend/src/services/marketService.js
+++ frontend/src/services/marketService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/market",
+  baseURL: import.meta.env.VITE_API_URL + "/market",
 });
 
 export const getMarketPrices = async (state, crop) => {
